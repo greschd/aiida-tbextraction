b37c9d3e9aab79ffb7caba4bba9a9e21b47cc4d0
Dominik Gresch <greschd@gmx.ch>
2017-08-16 10:29:29 +0200

    Add __version__

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
-"""
-setup: usage: pip install -e .
-"""
 
+import re
 from setuptools import setup, find_packages
+
+# Get the version number
+with open('./aiida_tbextraction/__init__.py') as f:
+    match_expr = "__version__[^'\"]+(['\"])([^'\"]+)"
+    version = re.search(match_expr, f.read()).group(2).strip()
 
 if __name__ == '__main__':
     setup(
         name='aiida-tbextraction',
-        version='0.0.0a1',
+        version=version,
         description='AiiDA Plugin for extracting tight-binding models',
         author='Dominik Gresch',
         author_email='greschd@gmx.ch',
