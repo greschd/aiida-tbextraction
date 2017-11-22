96c20d4e10da89fb93ce7eb6141cf675f0c6a953
Dominik Gresch <greschd@gmx.ch>
2017-11-22 15:44:19 +0100

    Add bands_inspect.plot calculation to model_evaluation.band_difference

--- aiida_tbextraction/model_evaluation/_band_difference.py
+++ aiida_tbextraction/model_evaluation/_band_difference.py
@@ -1,3 +1,7 @@
+"""
+Defines a workflow which evaluates a tight-binding model by comparing its bandstructure to a reference bandstructure.
+"""
+
 from fsc.export import export
 
 from aiida.orm import CalculationFactory
@@ -27,10 +31,14 @@ class BandDifferenceModelEvaluation(ModelEvaluation):
         )
 
         spec.outline(
-            cls.calculate_bands, cls.calculate_difference, cls.finalize
+            cls.calculate_bands, cls.calculate_difference_and_plot,
+            cls.finalize
         )
 
     def setup_calc(self, calc_string, code_param):
+        """
+        Helper function to set up a calculation of a specified type.
+        """
         process = CalculationFactory(calc_string).process()
         inputs = process.get_inputs_template()
         inputs.code = self.inputs[code_param]
@@ -40,6 +48,9 @@ class BandDifferenceModelEvaluation(ModelEvaluation):
 
     @check_workchain_step
     def calculate_bands(self):
+        """
+        Calculate the bandstructure of the given tight-binding model.
+        """
         process, inputs = self.setup_calc(
             'tbmodels.eigenvals', 'tbmodels_code'
         )
@@ -50,16 +61,29 @@ class BandDifferenceModelEvaluation(ModelEvaluation):
         return ToContext(calculated_bands=pid)
 
     @check_workchain_step
-    def calculate_difference(self):
-        process, inputs = self.setup_calc(
+    def calculate_difference_and_plot(self):
+        """
+        Calculate the difference between the tight-binding and reference bandstructures, and plot them.
+        """
+        process_diff, inputs = self.setup_calc(
             'bands_inspect.difference', 'bands_inspect_code'
         )
+        process_plot, _ = self.setup_calc(
+            'bands_inspect.plot', 'bands_inspect_code'
+        )
+        # Inputs for the plot and difference calculations are the same
         inputs.bands1 = self.inputs.reference_bands
         inputs.bands2 = self.ctx.calculated_bands.out.bands
         self.report('Running difference calculation.')
-        pid = submit(process, **inputs)
-        return ToContext(difference=pid)
+        pid_diff = submit(process_diff, **inputs)
+        self.report('Running plot calculation.')
+        pid_plot = submit(process_plot, **inputs)
+        return ToContext(difference=pid_diff, plot=pid_plot)
 
     @check_workchain_step
     def finalize(self):
+        """
+        Return outputs of the difference and plot calculations.
+        """
         self.out('cost_value', Float(self.ctx.difference.out.difference))
+        self.out('plot', self.ctx.plot.out.plot)
